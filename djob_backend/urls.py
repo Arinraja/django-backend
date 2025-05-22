b447932f1d1dd130cbddb474accfcdcd3990ec70
Arin raja <rajansar571@gmail.com>
2025-05-23 02:13:02 +0530

    Update urls.py

--- djob_backend/urls.py
+++ djob_backend/urls.py
@@ -33,10 +33,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/jobs/', include('job.urls')),
+    
+        path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     # Djoser auth endpoints (registration, reset, etc.)
     path('api/v1/auth/', include('djoser.urls')),
@@ -44,7 +55,7 @@ urlpatterns = [
     # JWT token views
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    # Redirect root URL to /api/v1/jobs/ or any other path you want
     # Job app endpoints
     path('api/v1/jobs/', include('job.urls')),
 
