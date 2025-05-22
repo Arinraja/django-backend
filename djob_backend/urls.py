63f5988b48d812851b7d5b5797cb6481b42b9f02
Arin raja <rajansar571@gmail.com>
2025-05-23 02:36:30 +0530

    Update urls.py

--- djob_backend/urls.py
+++ djob_backend/urls.py
@@ -9,27 +9,27 @@
 # ]
 
 
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.views.generic import RedirectView
-# from django.urls import path
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/v1/', include('djoser.urls')),
-#     path('api/v1/', include('djoser.urls.authtoken')),
-#     path('api/v1/jobs/', include('job.urls')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/jobs/', include('job.urls')),
     
-#         path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-#     # Redirect root URL to /api/v1/jobs/ or any other path you want
-#     path('', RedirectView.as_view(url='/api/v1/jobs/', permanent=False)),
-# ]
+    # Redirect root URL to /api/v1/jobs/ or any other path you want
+    path('', RedirectView.as_view(url='/api/v1/jobs/', permanent=False)),
+]
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -39,15 +39,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-    path('api/v1/jobs/', include('job.urls')),
+    # path('api/v1/jobs/', include('job.urls')),
     
-        path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     # Djoser auth endpoints (registration, reset, etc.)
     path('api/v1/auth/', include('djoser.urls')),
@@ -57,7 +57,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # Redirect root URL to /api/v1/jobs/ or any other path you want
     # Job app endpoints
-    path('api/v1/jobs/', include('job.urls')),
+    # path('api/v1/jobs/', include('job.urls')),
 
     # Redirect root to jobs API
     path('', RedirectView.as_view(url='/api/v1/jobs/', permanent=False)),
